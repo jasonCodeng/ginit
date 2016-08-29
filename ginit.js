b3374db8ad6f67b369914953c7126f380cd8257d
Jason Deng <jdeng1234@gmail.com>
2016-08-29 18:36:08 -0400

    added Ginit ui and prompt for github credentials

--- ginit.js
+++ ginit.js
@@ -1,9 +1,8 @@
 /**
- *  Require dependencies
+ * Require npm dependencies
  */
-
 var chalk       = require('chalk');
-var clear       = require('clear');
+var clear       = require("cli-clear");
 var CLI         = require('clui');
 var figlet      = require('figlet');
 var inquirer    = require('inquirer');
@@ -14,3 +13,67 @@ var _           = require('lodash');
 var git         = require('simple-git')();
 var touch       = require('touch');
 var fs          = require('fs');
+
+/**
+ * Require own dependencies
+ */
+ var files = require('./lib/files');
+
+ /**
+  * Clear console
+  */
+clear();
+
+/**
+ * Print 'Ginit' ASCII art in green.
+ */
+console.log(
+  chalk.green(
+    figlet.textSync('Ginit', {
+      font: 'Standard',
+      horizontalLayout: 'default',
+      verticalLayout: 'default'
+    })
+  )
+);
+
+/**
+* Check if current directory has a .git directory
+*/
+/* COMMENTED OUT FOR TESTING PURPOSES
+if(files.checkDirectoryExists('.git')) {
+  console.log(chalk.red('This directory is already a git repository!'))
+  process.exit();
+}
+*/
+
+function getGithubCredentials(callback) {
+  var questions = [
+    {
+      name: 'username',
+      type: 'input',
+      message: 'Enter your Github username:',
+      validate: function(input) {
+        //  add better validation
+        if (input.length) {
+          return true;
+        } else {
+          return 'Please enter your username';
+        }
+      }
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'Enter your password:',
+      validate: function(input) {
+        if (input.length) {
+          return true;
+        } else {
+          return 'Please enter your passwprd';
+        }
+      }
+    }
+  ];
+  inquirer.prompt(questions).then(callback);
+}
