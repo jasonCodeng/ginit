14cbd3f9a7fe2d89f4a3c9dea81c01eaf95cd13d
Jason Deng <jdeng1234@gmail.com>
2016-08-29 20:54:26 -0400

    Initial commit

--- ginit.js
+++ ginit.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /**
  * Require npm dependencies
  */
@@ -81,7 +83,7 @@ function getGithubCredentials(callback) {
         if (input.length) {
           return true;
         } else {
-          return 'Please enter your passwprd';
+          return 'Please enter your password';
         }
       }
     }
@@ -97,6 +99,7 @@ function getGithubToken(callback) {
 
   /**
   * Check if 'ginit' perferences file already has a Github token in storage
+  * TODO: add two step authentcation
   */
   if (prefs.github && prefs.github.token) {
     return callback(null, prefs.github.token);
@@ -119,7 +122,7 @@ function getGithubToken(callback) {
     github.authenticate(
       _.extend(
         {
-          type: 'basic',
+          type: 'basic'
         },
         credentials
       )
@@ -129,10 +132,10 @@ function getGithubToken(callback) {
     * Create access token for our application on current machine
     */
     github.authorization.create({
-        scopes: ['user', 'public_repo', 'repo', 'repo::status'],
+        scopes: ['user', 'public_repo', 'repo', 'repo:status'],
         note: 'ginit, the super powered git init'
     }, function(err, res) {
-        status.stop();
+        spinner.stop();
         if (err) {
             return callback(err);
         }
@@ -146,3 +149,150 @@ function getGithubToken(callback) {
     });
   });
 }
+/*
+getGithubToken(function(){
+  console.log(arguments);
+});
+*/
+function createRepo(callback) {
+  var argv = require('minimist')(process.argv.slice(2));
+
+  var questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter a name for the repository:',
+      default: argv._[0] || files.getCurrentWorkingDirectory(),
+      validate: function(input) {
+        if (input.length) {
+          return true;
+        } else {
+          return 'Please enter a name for the repository';
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      default: argv._[1] || null,
+      message: 'Optionally enter a description of the repository:'
+    },
+    {
+      type: 'list',
+      name: 'visibility',
+      message: 'Is this a public or private repository?',
+      choices: ['Public', 'Private'],
+      default: 'Public'
+    }
+  ];
+
+  inquirer.prompt(questions).then(function(answers) {
+    var spinner = new Spinner('Creating repository...');
+    spinner.start();
+
+    var data = {
+      name: answers.name,
+      description: answers.description,
+      private: (answers.visibility === 'private')
+    };
+
+    github.repos.create(
+      data,
+      function(err,res) {
+        spinner.stop();
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, res.ssh_url);
+      }
+    )
+  })
+}
+
+function createGitIgnore(callback) {
+  var filelist = _.without(fs.readdirSync('.'), '.git', '.gitignore');
+
+  if (filelist.length) {
+    inquirer.prompt(
+      [
+        {
+          type: 'checkbox',
+          name: 'ignore',
+          message: 'Select the files and/or folders you would to ignore:',
+          choices: filelist,
+          default: ['node_modules', 'bower_components']
+        }
+      ]
+    ).then(function(answers) {
+      if (answers.ignore.length) {
+        fs.writeFileSync('.gitignore', answers.ignore.join('\n'));
+      } else {
+        touch('.gitignore');
+      }
+      return callback();
+    })
+  } else {
+    touch('.gitignore');
+    return callback();
+  }
+}
+
+function setupRepo(url, callback) {
+  var spinner = new Spinner('Setting up the repository...');
+  spinner.start();
+
+  git
+    .init()
+    .add('.gitignore')
+    .add('./*')
+    .commit('Initial commit')
+    .addRemote('origin', url)
+    .push('origin', 'master')
+    .then(function() {
+      spinner.stop();
+      return callback();
+    });
+}
+
+function githubAuth(callback) {
+  getGithubToken(function(err, token) {
+    if (err) {
+      return callback(err);
+    }
+    github.authenticate({
+      type: 'oauth',
+      token: token
+    });
+    return callback(null, token);
+  });
+}
+
+githubAuth(function(err, authed) {
+  if (err) {
+    switch (err.code) {
+      case 401:
+        console.log(chalk.red('Couldn\'t log in. Please try again.'));
+        break;
+      case 422:
+        console.log(chalk.red('You already have an access token.'));
+        break;
+    }
+  }
+  if (authed) {
+    console.log(chalk.green('Successfully authenticated!'));
+    createRepo(function (err, url) {
+      if (err) {
+        console.log('An error has occured');
+      }
+      if (url) {
+        createGitIgnore(function() {
+          setupRepo(url, function(err) {
+            if(!err) {
+              console.log(chalk.green('Finished!'));
+            }
+          });
+        });
+      }
+    });
+  }
+});
